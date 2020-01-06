69a1152a5836803daeecbb56f17eb18b51da9faf
charleytran2001 <44280830+charleytran2001@users.noreply.github.com>
2020-01-06 10:59:06 -0700

    Made it so you can see your cards
    Instead of just being able to see the total, you can now see the cards you have

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var user_hand = 0;
-var dealer_hand = 0;
+var user_hand_total = 0;
+var user_hand = "";
+var dealer_hand_total = 0;
+var dealer_hand = "";
 var charlie_counter = 1;
 
 starting_hand();
@@ -10,35 +12,39 @@ function starting_hand() {
 }
 
 function hit() {
-  user_hand += Math.floor(Math.random() * 10 + 1);
-  document.getElementById("user_hand").innerHTML = "Your Hand: " + user_hand;
+  var user_random = Math.floor(Math.random() * 10 + 1);
+  user_hand_total += user_random;
+  user_hand += user_random + " ";
+  document.getElementById("user_hand_total").innerHTML = "Your Hand: " + user_hand_total;
   
-  if(user_hand > 21) {
+  if(user_hand_total > 21) {
     dealer();
-    document.getElementById("dealer_hand").innerHTML = "Dealer's Hand: " + dealer_hand;
+    document.getElementById("dealer_hand_total").innerHTML = "Dealer's Hand: " + dealer_hand_total;
     document.getElementById("status").innerHTML = "You Lose";
   }
   
-  if(user_hand === 21) {
+  if(user_hand_total === 21) {
     stand();
   }
   
-  if(charlie_counter === 6 && user_hand <= 21) {
+  if(charlie_counter === 6 && user_hand_total <= 21) {
     dealer();
-    document.getElementById("dealer_hand").innerHTML = "Dealer's Hand: " + dealer_hand;
+    document.getElementById("dealer_hand_total").innerHTML = "Dealer's Hand: " + dealer_hand_total;
     document.getElementById("status").innerHTML = "6-Card Charlie: You Win";
   }
+  
+  document.getElementById("user_hand").innerHTML = user_hand;
   
   charlie_counter++;
 }
 
 function stand() {
   dealer();
-  document.getElementById("dealer_hand").innerHTML = "Dealer's Hand: " + dealer_hand;
+  document.getElementById("dealer_hand_total").innerHTML = "Dealer's Hand: " + dealer_hand_total;
   
-  if(user_hand > dealer_hand) {
+  if(user_hand_total > dealer_hand_total) {
     document.getElementById("status").innerHTML = "You Win";
-  } else if(user_hand < dealer_hand) {
+  } else if(user_hand_total < dealer_hand_total) {
     document.getElementById("status").innerHTML = "You Lose";
   } else {
     document.getElementById("status").innerHTML = "It's a Tie";
@@ -47,11 +53,14 @@ function stand() {
 
 function dealer() {
   var check;
-  while(dealer_hand <= 21) {
-    var deal = Math.floor(Math.random() * 10 + 1);
-    dealer_hand += deal;
-    console.log(dealer_hand);
+  while(dealer_hand_total <= 21) {
+    var dealer_random = Math.floor(Math.random() * 10 + 1);
+    var deal = dealer_random;
+    dealer_hand_total += deal;
+    dealer_hand += dealer_random + " ";
     check = deal;
   }
-  dealer_hand -= check;
+  dealer_hand_total -= check;
+  
+  document.getElementById("dealer_hand").innerHTML = dealer_hand;
 }
